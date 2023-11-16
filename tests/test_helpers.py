a3013775b62aae51ce9fb47318321eef784627e3
Sifan Wei <sifanw1998@gmail.com>
2023-11-16 13:10:08 -0800

    Added tests

---
+++ tests/test_helpers.py
@@ -0,0 +1,17 @@
+import pytest
+from starlette.exceptions import HTTPException
+from routers.helpers import check_user_authentication  
+
+def test_check_user_authentication_with_user():
+    user = {"username": "testuser", "id": 1}  
+    try:
+        check_user_authentication(user)
+        assert True  
+    except HTTPException:
+        pytest.fail("HTTPException was raised unexpectedly")
+
+def test_check_user_authentication_without_user():
+    with pytest.raises(HTTPException) as excinfo:
+        check_user_authentication(None)
+    assert excinfo.value.status_code == 401
+    assert "Authentication Failed" in str(excinfo.value)
